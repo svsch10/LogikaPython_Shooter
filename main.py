4125e22f6f10d94f8250d48862ffc2fa93cf2916
oleksandr_s <62945038+svsch10@users.noreply.github.com>
2026-08-09 13:40:52 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,58 @@
 from kivymd.app import MDApp
 from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.widget import MDWidget
 from kivymd.uix.screen import MDScreen
 from kivy import platform
 from kivy.core.window import Window
+from kivy.clock import Clock
+from kivy.metrics import sp, dp
+
+FPS = 60
+SHIP_SPEED = dp(5)
+BULLET_SPEED = dp(10)
 
 class MainScreen(MDScreen):
     ...
 
-class GameScreen(MDScreen):
+class Shot(MDWidget):
     ...
 
+class GameScreen(MDScreen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        Clock.schedule_interval(self.update, 1/FPS)
+        self.cartridge = []
+        self.eventkeys = {}
+    
+    def update(self, dt):
+        for key in self.eventkeys:
+            if self.eventkeys[key] == True:
+                if key == 'left':
+                    self.moveLeft()
+                if key == 'right':
+                    self.moveRight()
+                if key == 'shot':
+                    self.shot()
+                    self.eventkeys[key] = False
+                    
+        for bullet in self.cartridge:
+            bullet.pos[1] += BULLET_SPEED
+    
+    def pressKey(self, key):
+        self.eventkeys[key] = True
+    def releaseKey(self, key):
+        self.eventkeys[key] = False
+    def moveLeft(self):
+        self.ids.ship.pos[0] -= SHIP_SPEED
+    def moveRight(self):
+        self.ids.ship.pos[0] += SHIP_SPEED
+    def shot(self):
+        shot = Shot(pos=(self.ids.ship.center_x,
+                         self.ids.ship.top))
+        self.cartridge.append(shot)
+        self.ids.front.add_widget(shot)
+        
+        
 class ShooterApp(MDApp):
     def build(self):
         # --- Стиль застосунку: світлий чи темний ---
@@ -25,7 +68,7 @@ class ShooterApp(MDApp):
         return self.sm
 
 if platform != 'android':
-    Window.size = (450, 900)
+    Window.size = (450, 700)
     Window.top = 100
     Window.left = 600
      
